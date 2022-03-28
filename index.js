942f352024c235b7ca13b5391a049a65416d1123
yasminkasem66 <yasmin.abdellh@gmail.com>
2022-03-28 18:24:45 +0200

    Course intro

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("ccc");
+// function is a first class citizien
+// we can use function to store in a variable
+const calc=()=>{
+    return 4*5;
+}
+// a variable that hold that functions
+
+// 1_1
+// let aNumber=calc();
+// console.log(aNumber);
+
+// 1_2
+// callback function is a function that is called inside another function 
+const printCalc=(calc)=> console.log(calc());
+printCalc(calc);
